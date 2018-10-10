23b649850e2314516b64aa621770fca45b400688
user143154 <user143154@87-237-62-1.rdns.melbourne.co.uk>
2018-10-10 16:50:44 +0100

    PLA 2 - Ejercicio 1

--- MyPlaces/DetailController.swift
+++ MyPlaces/DetailController.swift
@@ -8,21 +8,24 @@
 
 import UIKit
 
-class DetailController: UIViewController {
-
-    @IBOutlet weak var constraintHeight: NSLayoutConstraint!
-    
+class DetailController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+ 
     @IBOutlet weak var viewPicker: UIPickerView!
     @IBOutlet weak var imagePicked: UIImageView!
     @IBOutlet weak var textName: UITextField!
     @IBOutlet weak var textDescription: UITextView!
     
     var place:Place? = nil
+    
+    // Places types
+    let pickerElems1 = ["Generic place", "Touristic place"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        self.constraintHeight.constant = 400
+        
+        viewPicker.delegate = self
+        viewPicker.dataSource = self
+	
         // Do any additional setup after loading the view.
         
         if(place != nil){
@@ -53,10 +56,53 @@ class DetailController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
+    
+     @IBAction func Update(_ sender: Any) {
+     }
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     @IBAction func Delete(_ sender: Any) {
+     }
+     
+     func numberOfComponents(in pickerView: UIPickerView) -> Int
+     {
+     return 1
+     }
+     
+     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+     {
+     return pickerElems1.count
+     }
+     
+     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+     {
+     return pickerElems1[row]
+     }
+    
+    /*
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView
+    {
+        let pickerLabel = UILabel()
+        
+        pickerLabel.text = pickerElems1[row]
+        
+        if row == 0
+        {
+            pickerLabel.textColor = UIColor.black
+            pickerLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        }
+        else
+        {
+            pickerLabel.textColor = UIColor.gray
+            pickerLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        }
+        
+        pickerLabel.textAlignment = NSTextAlignment.center
+        return pickerLabel
+    }
+     */ 
+     /*
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
